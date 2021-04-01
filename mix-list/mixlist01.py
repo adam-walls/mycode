ede3c07fc1100b44fc3e027135ddeb5229bfb208
Adam Walls <awalls1@gmail.com>
2021-04-01 18:33:21 +0000

    mixed lists

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -12,8 +12,10 @@ print("The second item in the list (port): " + str(my_list[1]) )
 # display the third item in the list
 print("The last item in the list (state): " + my_list[2] )
 
+# New list of values
 #             0     1    2      3           4            5
 new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
-print("When I", new_list[5], "into IP addresses",  new_list[3],  "or", new_list[4], "I am unable to ping ports ", str(new_list[0]), ",", new_list[1], "or", str(new_list[2]) )
+# Display the new sentence with list[] values
+print("When I", new_list[5], "into IP addresses", new_list[3], "or", new_list[4], "I am unable to ping ports", str(new_list[0]), ",", new_list[1], "or", str(new_list[2]) )
 
