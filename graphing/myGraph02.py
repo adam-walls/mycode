93554b5e37428264e73ae3110f0942d3a113ba42
Adam Walls <awalls1@gmail.com>
2021-04-08 15:06:13 +0000

    my line graph

---
+++ graphing/myGraph02.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+ax.grid()
+
+# SAVE the graph locally
+plt.savefig("/home/student/mycode/graphing/myGraph02PNG.png")
+# Save to "~/static"
+plt.savefig("/home/student/static/myGraph02PNG.png")       
+print("Graph created.")
+
+fig.savefig("test.png")
+plt.show()
